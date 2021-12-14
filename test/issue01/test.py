6799afb8eb6e89f662213d420f797a8d462f6b75
azizaoce <60894980+azizaoce@users.noreply.github.com>
2021-12-14 20:44:31 +0300

    Add files via upload

--- test/issue01/test.py
+++ test/issue01/test.py
@@ -4,31 +4,30 @@ from morse import encode
 
 
 def test_decode(string):
-   """
-   morse to letter
-   >>> test_decode('-- .- .. -....- .--. -.-- - .... --- -. -....- ..--- ----- .---- ----.')
-   'MAI-PYTHON-2019'
-   """
-   return decode(string)
+    """
+    morse to letter
+    >>> test_decode('-- .- .. -....- .--. -.-- - .... --- -. -....- ..--- ----- .---- ----.')
+    'MAI-PYTHON-2019'
+    """
+    return decode(string)
 
 
 def test_encode(string):
-   """
-   letter to morse
-   >>> test_encode('SOS')
-   '... --- ...'
-   >>> test_encode('P' * 30) #doctest: +ELLIPSIS
-   '.--. ... .--.'
-   >>> test_encode('S   O   S') # doctest: +NORMALIZE_WHITESPACE
-   '... --- ...'
-   >>> test_encode(30.1)
-   Traceback (most recent call last):
+    """
+    letter to morse
+    >>> test_encode('SOS')
+    '... --- ...'
+    >>> test_encode('P' * 30) #doctest: +ELLIPSIS
+    '.--. ... .--.'
+    >>> test_encode('S   O   S') # doctest: +NORMALIZE_WHITESPACE
+    '... --- ...'
+    >>> test_encode(30.1)
+    Traceback (most recent call last):
         ...
-   TypeError: 'float' object is not iterable
-   """
-   return encode(string)
+    TypeError: 'float' object is not iterable
+    """
+    return encode(string)
 
 
 if __name__ == '__main__':
-   doctest.testmod()
-   
+    doctest.testmod()
